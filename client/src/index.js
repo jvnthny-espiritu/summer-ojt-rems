f1aaa0a7a25a1d3ad5bac1b02a6c88a44f817437
Seth Arkin Cruzat <sethscruzat@gmail.com>
2024-06-24 10:38:18 +0800

    basic login + basic routing

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
+import Login from "./pages/login/Login";
+import Home from "./pages/home/Home";
 import reportWebVitals from './reportWebVitals';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          {/* <Route path="*" element={<NoPage />} /> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
