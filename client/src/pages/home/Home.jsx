042ca10b03d6160a10da6505894869fb96d6c422
Jenloke Magbojos <mjenloke@gmail.com>
2024-06-24 11:12:02 +0800

    add tailwind configs, dashboard basic html, raname js to jsx

---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,41 @@
+const Home = () => {
+  return (
+    <>
+      <div>
+        <h2>
+          3D Printers
+        </h2>
+        <ul>
+          <li>Ultimaker</li>
+          <li>Ultimaker</li>
+          <li>Creality</li>
+        </ul>
+      </div>
+
+      <div>
+        <h2>
+          CNC Machines        
+        </h2>
+        <ul>
+          <li>Ultimaker</li>
+          <li>Ultimaker</li>
+          <li>Creality</li>
+        </ul>
+      </div>
+
+      <div>
+        <h2>
+          CNC Machines        
+        </h2>
+        <ul>
+          <li>Ultimaker</li>
+          <li>Ultimaker</li>
+          <li>Creality</li>
+        </ul>
+      </div>
+
+    </>   
+  );
+};
+
+export default Home;
