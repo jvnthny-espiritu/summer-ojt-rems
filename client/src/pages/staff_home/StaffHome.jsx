dd6ace0f932052d67bd44337d82f9f260c071360
Jenloke Magbojos <mjenloke@gmail.com>
2024-07-01 16:44:29 +0800

    ui updates

--- client/src/pages/staff_home/StaffHome.jsx
+++ client/src/pages/staff_home/StaffHome.jsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, {useEffect, useState} from 'react';
 import DeviceInfo from "./DeviceInfo";
 import AddDevice from "./AddDevice";
 import api from '../../services/api';
-import { useParams, useLoaderData } from "react-router-dom"
-import { jwtDecode } from 'jwt-decode';
+import {useLoaderData} from "react-router-dom"
+import {jwtDecode} from 'jwt-decode';
 
 const Pagination = ({ currentPage, totalPages, onPageChange, type, userId }) => {
 	const pageNumbers = [];
@@ -11,14 +11,14 @@ const Pagination = ({ currentPage, totalPages, onPageChange, type, userId }) => 
 		pageNumbers.push(i);
 	}
 	return (
-		<div className="pagination flex flex-row gap-10 ml-6">
+		<div className="pagination flex flex-row gap-3 ml-6">
 			{pageNumbers.map((number) => (
 				<button
 					key={number}
 					onClick={() => onPageChange(userId,type,number)}
 					className={`border-2 rounded-full px-2 py-1 text-sm border-black bg-zinc-300 opacity-75 ${number === currentPage ? 'active' : ''}`}
 				>
-				{number}
+					{number}
 				</button>
 			))}
 		</div>
@@ -27,7 +27,7 @@ const Pagination = ({ currentPage, totalPages, onPageChange, type, userId }) => 
 
 const StaffHome = () => {
 	const classNames = require('classnames')
-	
+
 	const userId = useLoaderData();
 	const token = localStorage.getItem('token')
 	const decodedToken = jwtDecode(token)
@@ -37,7 +37,7 @@ const StaffHome = () => {
 
 	const [error, setError] = useState('');
 	let [typesList, setTypesList] = useState([]);
-    const [equipmentList, setEquipmentList] = useState({});
+  const [equipmentList, setEquipmentList] = useState({});
 	const [refreshState, setRefreshState] = useState(false);
 	const [newDeviceOpenState, setNewDeviceOpenState] = useState({});
 
@@ -72,7 +72,7 @@ const StaffHome = () => {
 		setCurrentEquipment(null);
 		setOpen(false);
 	};
-	
+
 	const fetchTypes = async (id,e) => {
 		try {
             const response = await api.get(`api/research-centers/${id}/equipments`);
@@ -93,7 +93,7 @@ const StaffHome = () => {
 
 		return current;
 	}, {});
-	
+
 	const fetchEquipmentList = async (userId,type,pageId,e) =>{
         try {
 			const response = await api.get(`api/research-centers/${userId}/equipments?type=${type}&page=${pageId}&limit=5`);
@@ -107,16 +107,16 @@ const StaffHome = () => {
     useEffect(() => {
 		fetchTypes(userId);
 	}, [userId]);
-	
+
 	useEffect(() => {
 		// TODO reduce code
 		const types = typesList.reduce((current, equipment) => {
 			if (!current[equipment.type]) {
 				current[equipment.type] = [];
 			}
-	
+
 			current[equipment.type].push(equipment);
-	
+
 			return current;
 		}, {});
 		const tempList = Object.keys(types)
@@ -132,7 +132,7 @@ const StaffHome = () => {
 				setTotalPages(pages)
 				setEquipmentList(items);
 		};
-	
+
 		if (tempList.length > 0) {
 			fetchData();
 		}
@@ -162,21 +162,24 @@ const StaffHome = () => {
 		}));
 	};
 
-	// const { researchcenterid } = useParams();
+	// const { researchcenterid } = useParams(); // TO BE CHANGE TO LOADERS SINCE ROUTING IS USING ID
 
 	return(
 		// TODO make dynamic
-		<div className= "flex flex-col min-w-screen min-h-screen">
+		<div className= "flex flex-col min-w-screen min-h-screen font-base">
 			<div className="flex flex-col bg-red-800 p-10">
-				<p className="text-white text-2xl">STEERHub REM {"> " + decodedToken.name }</p>
+				<p className="text-white text-4xl text-justify">
+					{`STEERHub REM: ${decodedToken.name}`}
+				</p>
 				{/* <p className="text-white text-md">{decodedToken.code}</p> */}
 			</div>
+			
 			<div className="flex flex-col p-6">
 				{/*Should be a Component*/}
 				<div className="flex flex-col md:flex-row gap-5 md:gap-20 place-content-center text-lg">
 					<div className="text-center">
 						<p>Humidity</p>
-						<p>42°</p>
+						<p>42°</p>	
 					</div>
 					<div className="text-center">
 						<p>Temperature</p>
@@ -188,35 +191,19 @@ const StaffHome = () => {
 					</div>
 				</div>
 
-				{Object.keys(equipmentTypes).map((item) => (
-					<div key={item}>
-						<h3 className="text-lg mt-8 font-bold">{item}</h3>
-						<div className="flex p-6">
-							<ul className="flex flex-col gap-5 w-full justify-between md:flex-row">
-								{equipmentList[item] && equipmentList[item].map((equipment) => (
-									<li key={equipment.id}>
-										<button 
-											className={classNames(
-												"flex p-1 rounded-md text-white text-lg",{
-													"bg-green-800": equipment.status==="available",
-													"bg-red-800": equipment.status==="for repair",
-													"bg-orange-600": equipment.status==="work-in-progress"
-												} 
-											)}
-											onClick={() => handleOpen(equipment)}>{equipment.model}</button>
-										{currentEquipment && (
-											<DeviceInfo 
-												open={openState} 
-												handleClose={handleClose} 
-												equipment={currentEquipment ? currentEquipment : ""}
-												handleChangesAfterClose={handleChangesAfterClose}/>
-										)}
-									</li>
-								))}
-								<li>
-									<button 
-										className="flex p-1 rounded-md text-white text-lg bg-black"
-										onClick={() => toggleCreationModal(item)}>ADD
+				<div className="flex flex-col gap-5">
+					{Object.keys(equipmentTypes).map((item) => (
+						<div key={item}>
+							<div className="flex gap-3 p-5 justify-between">
+								<div className="self-center text-3xl font-bold">
+									{item}
+								</div>
+								<div className="self-center text-center">
+									<button
+										className="flex py-1 px-3 rounded-md text-white text-lg bg-black"
+										onClick={() => toggleCreationModal(item)}
+									>
+										ADD Equipmentx
 									</button>
 									<AddDevice
 										handleOpenState = {() => toggleCreationModal(item)}
@@ -225,20 +212,53 @@ const StaffHome = () => {
 										rcId = {userId}
 										handleCreation = {handleCreation}
 									/>
-								</li>
-							</ul>
-						</div>
-						<div className="flex flex-row">
-							<Pagination
+								</div>
+							</div>
+
+							<div className="p-6">
+								<ul className="flex flex-col md:flex-row gap-5 w-full justify-evenly">
+									{equipmentList[item] && equipmentList[item].map((equipment) => (
+										<li key={equipment.id}>
+											<button
+												className={classNames(
+													"p-1 rounded-md text-white text-lg",
+													{
+														"bg-green-800": equipment.status==="available",
+														"bg-red-800": equipment.status==="for repair",
+														"bg-orange-600": equipment.status==="work-in-progress"
+													}
+												)}
+												onClick={() => handleOpen(equipment)}
+											>
+												{equipment.model}
+												<hr />
+												{equipment.status}
+											</button>
+
+											{currentEquipment && (
+												<DeviceInfo
+													open={openState}
+													handleClose={handleClose}
+													equipment={currentEquipment ? currentEquipment : ""}
+													handleChangesAfterClose={handleChangesAfterClose}
+												/>
+											)}
+										</li>
+									))}
+								</ul>
+							</div>
+							<div className="flex flex-row justify-end">
+								<Pagination
 									currentPage={currentPage}
 									totalPages={totalPages[item]}
 									onPageChange={handlePageChange}
 									type={item}
 									userId={userId}
-							/>
+								/>
+							</div>
 						</div>
-					</div>
-				))}
+					))}
+				</div>
 			</div>
 		</div>
 	)
